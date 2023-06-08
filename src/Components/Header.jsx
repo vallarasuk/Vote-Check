2b0d3744bc598451e2b8ab86afc716406fa1cd28
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-06-08 19:35:19 +0530

    function change commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,77 +1,84 @@
 import React, { useState } from "react";
 
 const Header = () => {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+  const [userValidate, setUserValidate] = useState(false);
   const [userRespons, setUserRespons] = useState("");
   const [colors, setColors] = useState("");
+
   const handleSubmit = (event) => {
-    let userName = document.getElementById("name").value;
-    let userAge = document.getElementById("age").value;
-    let userValidate = document.getElementById("valid").checked;
-    let allowedAge = 18;
+    event.preventDefault();
+
+    if (isNaN(userAge) || userAge < 0 || userAge > 110) {
+      setUserRespons("Please enter a valid age");
+      setColors("red");
+      return;
+    }
 
     if (userName === "") {
-      setUserRespons(`Hello User please enter you name`);
+      setUserRespons("Hello User, please enter your name");
       setColors("red");
     } else if (userAge === "") {
-      setUserRespons("please enter you age");
+      setUserRespons("Please enter your age");
       setColors("red");
-    } else if (userAge && !userValidate) {
-      setUserRespons("please accept terms and conditions");
+    } else if (!userValidate) {
+      setUserRespons("Please accept the terms and conditions");
       setColors("red");
-    } else if (userAge >= 18 && userValidate === false) {
+    } else if (userAge >= 18 && !userValidate) {
       setUserRespons(
-        `Hi..! ${userName} Your Age ${allowedAge}, So Your are allow to vote, You Need to accept terms and conditions`
+        `Hi ${userName}! Your age is ${userAge}, so you are allowed to vote. You need to accept the terms and conditions.`
       );
       setColors("red");
-    } else if (userAge <= 18) {
+    } else if (userAge < 18) {
       setUserRespons(
-        `Hi..! ${userName} Your Age less than ${allowedAge}, So Your are not allow to vote`
+        `Hi ${userName}! Your age is less than 18, so you are not allowed to vote.`
       );
       setColors("red");
-    } else if (userAge >= 18 && userValidate === true && userName) {
+    } else if (userAge >= 18 && userValidate) {
       setUserRespons(
-        `Hi..! ${userName} Your Age ${userAge}, So Your are allow to vote`
+        `Hi ${userName}! Your age is ${userAge}, so you are allowed to vote.`
       );
       setColors("green");
     } else {
-      setUserRespons(`please enter you age and accept terms and condition`);
+      setUserRespons(
+        "Please enter your age and accept the terms and conditions"
+      );
       setColors("red");
     }
-
-    event.preventDefault();
   };
 
   return (
-    <div className="container my-3 my-sm-5 card py-4 px-3 shadow rounded-5 ">
+    <div className="container my-3 my-sm-5 card py-4 px-3 shadow rounded-5">
       <h5 className="text-center text-uppercase bg-info p-2 py-3 fw-bold text-white rounded-5 shadow">
-        Age based voting Calculation
+        Age-based Voting Calculation
       </h5>
-      
       <form onSubmit={handleSubmit} className="mx-3">
         <div className="text-start">
-          <div className=" text-center">
-            <div className="">
+          <div className="text-center">
+            <div>
               <input
                 type="text"
                 placeholder="Enter Your Name"
-                name="name"
-                id="name"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
                 style={{ fontSize: 18 }}
                 className="shadow border rounded-pill p-3 px-4 mt-3"
+                pattern="[A-Za-z ]+"
+                title="Please enter only text characters"
               />
             </div>
-            <div className="">
+            <div>
               <input
                 type="number"
                 placeholder="Enter Your Age"
-                name="age"
-                id="age"
+                value={userAge}
+                onChange={(e) => setUserAge(e.target.value)}
                 style={{ fontSize: 18 }}
-                maxLength={"100"}
+                maxLength={100}
                 className="shadow border px-4 rounded-pill p-3 mt-3"
               />
             </div>
-
             <h6
               className="fw-bold text-capitalize mt-3"
               id="response"
@@ -80,8 +87,16 @@ const Header = () => {
               {userRespons}
             </h6>
             <div>
-              <input type="checkbox" name="valid" id="valid" />
-              <span className="mx-2 h6 text-muted">Accept terms and conditions</span>
+              <input
+                type="checkbox"
+                name="valid"
+                id="valid"
+                checked={userValidate}
+                onChange={(e) => setUserValidate(e.target.checked)}
+              />
+              <span className="mx-2 h6 text-muted">
+                Accept terms and conditions
+              </span>
             </div>
             <button
               className="btn btn-primary p-2 text-uppercase fw-bold mt-3 shadow px-4 rounded-pill"
